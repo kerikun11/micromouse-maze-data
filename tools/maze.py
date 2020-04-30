9867cf4ed48ca3dbf8a9228258a6304f8a46985b
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-05-01 00:54:56 +0900

    add mazefiles

---
+++ tools/maze.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# python version >= 3.8
+# usage: $ python maze.py mazefile.maze
+# ============================================================================ #
+import sys
+import numpy as np
+
+
+class Maze:
+    """
+    a maze class includes wall data, start indexes, goal indexes
+    """
+    # constants
+    East, North, West, South = range(4)
+
+    def __init__(self, size=32):
+        """
+        construct a maze object with a maze size
+        """
+        self.size = size  # number of cells on one side of the maze
+        self.index_size = size * size * 2  # the number of walls to save: x * y * z
+        self.walls = np.zeros(self.index_size, dtype=bool)  # wall flags
+        self.knowns = np.zeros(self.index_size, dtype=bool)  # wall flags
+        self.start = []
+        self.goals = []
+
+    @staticmethod
+    def parse(file):
+        """
+        parse maze string and construct maze object
+        """
+        lines = file.readlines()
+        maze = Maze(len(lines)//2)  # get maze size
+        for i, line in enumerate(reversed(lines)):
+            line = line.rstrip()  # remove \n
+            # +---+---+
+            if i % 2 == 0:
+                for j, c in enumerate(line[2:: 4]):
+                    if c == '-':
+                        maze.wall(j, i//2, Maze.South, True, new_known=True)
+                    elif c == ' ':
+                        maze.wall(j, i//2, Maze.South, False, new_known=True)
+            # |   |   |
+            else:
+                for j, c in enumerate(line[0:: 4]):
+                    if c == '|':
+                        maze.wall(j, i//2, Maze.West, True, new_known=True)
+                    elif c == ' ':
+                        maze.wall(j, i//2, Maze.West, False, new_known=True)
+                for j, c in enumerate(line[2:: 4]):
+                    if c == 'S':
+                        maze.start.append([j, i//2])
+                    if c == 'G':
+                        maze.goals.append([j, i//2])
+        return maze
+
+    @classmethod
+    def uniquify(cls, x, y, d):
+        """
+        returns a unique coordinates of a wall without redundancy on both sides of the wall
+        """
+        if d == cls.East:
+            x, y, z, d = x, y, 0, cls.East
+        elif d == cls.North:
+            x, y, z, d = x, y, 1, cls.North
+        elif d == cls.West:
+            x, y, z, d = x-1, y, 0, cls.East
+        elif d == cls.South:
+            x, y, z, d = x, y-1, 1, cls.North
+        return x, y, z, d
+
+    def index(self, x, y, z):
+        """
+        get a unique id of a wall inside of the maze
+        """
+        if not self.is_inside_of_field(x, y, z):
+            raise ValueError("out of field!")
+        return z * self.size * self.size + y * self.size + x
+
+    def is_inside_of_field(self, x, y, z):
+        """
+        determine if the wall is inside of the field
+        """
+        s = self.size
+        return x >= 0 and y >= 0 and x < s+z-1 and y < s-z
+
+    def wall(self, x, y, d, new_state=None, new_known=None):
+        """
+        get wall or update wall, and optionally update known flag
+        """
+        x, y, z, d = self.uniquify(x, y, d)
+        if not self.is_inside_of_field(x, y, z):
+            return True
+        i = self.index(x, y, z)
+        if new_state != None:
+            self.walls[i] = new_state
+        if new_known != None:
+            self.knowns[i] = new_known
+        return self.walls[i]
+
+    def known(self, x, y, d, update=None):
+        """
+        get or update if the wall is known
+        """
+        x, y, z, d = self.uniquify(x, y, d)
+        if not self.is_inside_of_field(x, y, z):
+            return True
+        i = self.index(x, y, z)
+        if update != None:
+            self.knowns[i] = update
+        return self.knowns[i]
+
+    def __str__(self):
+        """
+        show information of the maze
+        """
+        return f'size: {self.size}x{self.size}' + '\n' + \
+            'start: ' + ', '.join([f'({x}, {y})' for x, y in self.start]) + '\n' + \
+            'goals: ' + ', '.join([f'({x}, {y})' for x, y in self.goals])
+
+    def generate_maze_string(self):
+        """
+        generate a string to be saved in text format
+        """
+        res = ''
+        for y in reversed(range(-1, self.size)):
+            # +---+---+
+            res += '+'
+            for x in range(self.size):
+                d = Maze.North
+                if not self.known(x, y, d):
+                    res += ' . '
+                elif self.wall(x, y, d):
+                    res += '---'
+                else:
+                    res += '   '
+                res += '+'
+            res += '\n'
+            # |   |   |
+            if y == -1:
+                break
+            res += '|'
+            for x in range(self.size):
+                if [x, y] in self.start:
+                    res += ' S '
+                elif [x, y] in self.goals:
+                    res += ' G '
+                else:
+                    res += '   '
+                d = Maze.East
+                if not self.known(x, y, d):
+                    res += '.'
+                elif self.wall(x, y, d):
+                    res += '|'
+                else:
+                    res += ' '
+            res += '\n'
+        return res
+
+
+# ============================================================================ #
+# example
+
+if __name__ == "__main__":
+    # default filepath for debug
+    filepath = './mazedata/32MM2019HX.maze'
+
+    # overwrite with commandline argument
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+
+    # read maze file
+    with open(filepath, 'r') as file:
+        maze = Maze.parse(file)
+    print(maze)
+    print(maze.generate_maze_string())
---
+++ tools/mazepainter.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# python version >= 3.8
+# usage: $ python maze.py mazefile.maze
+# ============================================================================ #
+import os
+import sys
+import datetime
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from maze import Maze  # calls ./maze.py
+
+
+class MazePainter:
+    """
+    paint a maze with matplotlib.pyplot
+    """
+
+    def __init__(self, maze):
+        self.maze = maze
+
+    def draw_maze(self):
+        maze = self.maze
+        for i in range(maze.size+1):
+            for j in range(maze.size):
+                # +---+---+
+                if maze.wall(i, j, Maze.South):
+                    self.draw_wall(maze, i, j, Maze.South)
+                else:
+                    self.draw_wall(maze, i, j, Maze.South, ':', color='gray')
+                # |   |   |
+                if maze.wall(j, i, Maze.West):
+                    self.draw_wall(maze, j, i, Maze.West)
+                else:
+                    self.draw_wall(maze, j, i, Maze.West, ':', color='gray')
+        for ps, t in [[maze.start, 'S'], [maze.goals, 'G']]:
+            for p in ps:
+                plt.text(p[0], p[1], t, ha='center', va='center')
+        plt.axes().set_aspect('equal')  # set the x and y axes to the same scale
+        plt.xticks(range(0, maze.size+1, 1))
+        plt.yticks(range(0, maze.size+1, 1))
+        plt.xlim([-1/2, maze.size-1/2])
+        plt.ylim([-1/2, maze.size-1/2])
+        plt.tight_layout()
+
+    @staticmethod
+    def draw_wall(maze, x, y, d, fmt='k', **kwargs):
+        x, y, z, d = Maze.uniquify(x, y, d)
+        if z == 0:
+            x, y = [x+1/2, x+1/2], [y-1/2, y+1/2]
+        else:
+            x, y = [x-1/2, x+1/2], [y+1/2, y+1/2]
+        plt.plot(x, y, 'w', lw=2)
+        plt.plot(x, y, fmt, **kwargs)
+        plt.plot(x, y, 'k.')
+
+    def attach_wall_toggle(self):
+        plt.connect('button_press_event', self.button_press_event)
+
+    def button_press_event(self, event):
+        maze = self.maze
+        x, y = event.xdata, event.ydata
+        xf, xi = math.modf(x)
+        yf, yi = math.modf(y)
+        z, d = [0, Maze.East] if abs(xf-1/2) < abs(yf-1/2) else [1, Maze.North]
+        if z == 0:
+            x, y = int(round(x-1/2)), int(round(y))
+        else:
+            x, y = int(round(x)), int(round(y-1/2))
+        w = not maze.wall(x, y, d)
+        c = 'r' if w else 'r:'
+        maze.wall(x, y, d, w, new_known=True)
+        MazePainter.draw_wall(maze, x, y, d, c)
+        plt.draw()
+        print(maze.generate_maze_string())
+
+
+# ============================================================================ #
+# example
+
+if __name__ == "__main__":
+    # default filepath for debug
+    filepath = './mazedata/32MM2019HX.maze'
+
+    # overwrite with commandline argument
+    if len(sys.argv) > 1:
+        filepath = sys.argv[1]
+
+    # read maze file
+    with open(filepath, 'r') as file:
+        maze = Maze.parse(file)
+    print(maze)
+    print(maze.generate_maze_string())
+
+    # prepare figure
+    fig = plt.figure(figsize=(10, 10))
+
+    # setup maze modifier
+    mp = MazePainter(maze)
+    mp.draw_maze()
+    mp.attach_wall_toggle()
+
+    # start
+    plt.show()
+
+    # save modified maze
+    os.makedirs('output', exist_ok=True)
+    datetime_string = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+    basename_without_ext = os.path.splitext(os.path.basename(filepath))[0]
+    output_filepath = f'./output/{basename_without_ext}-{datetime_string}.maze'
+    with open(output_filepath, 'w') as file:
+        file.write(maze.generate_maze_string())
+        print('modified maze data saved: ' + output_filepath)
