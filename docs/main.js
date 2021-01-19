cee2ad6134d4d5386d55d43683cf436c8d49d032
Ryotaro Onuki <kerikun11+github@gmail.com>
2021-01-19 18:23:06 +0900

    add maze download button

--- docs/main.js
+++ docs/main.js
@@ -7,9 +7,15 @@
 
 import Maze from "./maze.js";
 
-let maze_name = "maze_data";
+let maze_name = "default.maze";
 let maze_string = document.getElementById("maze-text-field").innerText;
 let maze = Maze.parse_maze_string(maze_string);
+
+function maze_name_escape(src) {
+    let maze_name = src.replaceAll(/[^\w_$]/ig, "_");
+    if (maze_name.match(/^[0-9]/)) maze_name = "_" + maze_name;
+    return maze_name;
+}
 
 /**
  * @brief C言語の配列フィールドの内容を更新する関数
@@ -19,7 +25,9 @@ function update_c_array() {
     if (document.getElementById("bit-order-1").checked) bit_order = [0, 1, 2, 3];
     if (document.getElementById("bit-order-2").checked) bit_order = [1, 0, 3, 2];
     let y_origin_is_top = document.getElementById("y-origin-is-top").checked;
-    let maze_c_array_string = maze.get_c_array_string({ name: maze_name, bit_order: bit_order, y_origin_is_top: y_origin_is_top });
+    let maze_c_array_string = maze.get_c_array_string({
+        name: maze_name_escape(maze_name), bit_order: bit_order, y_origin_is_top: y_origin_is_top
+    });
     document.getElementById("maze-c-array-field").innerText = maze_c_array_string;
 }
 
@@ -65,8 +73,7 @@ update_c_array();
 $("#maze-file-select-upload").on("change", function (evt) {
     // ラベルにファイル名を表示する
     $(this).next(".custom-file-label").html($(this)[0].files[0].name);
-    maze_name = $(this)[0].files[0].name.replaceAll(/[^\w_$]/ig, "_");
-    if (maze_name.match(/^[0-9]/)) maze_name = "_" + maze_name;
+    maze_name = $(this)[0].files[0].name
     //FileReaderの作成
     let reader = new FileReader();
     //テキスト形式で読み込む
@@ -85,8 +92,7 @@ $("#maze-file-select-upload").on("change", function (evt) {
 $("#maze-file-select-github").on("change", function (evt) {
     // ラベルにファイル名を表示する
     let url = $(this).val();
-    maze_name = url.split('/').reverse()[0].replaceAll(/[^\w_$]/ig, "_");
-    if (maze_name.match(/^[0-9]/)) maze_name = "_" + maze_name;
+    maze_name = url.split('/').reverse()[0]
     fetch(url).then(function (response) {
         response.text().then(function (text) {
             maze = Maze.parse_maze_string(text);
@@ -122,6 +128,24 @@ $("#maze-c-array-copy-button").on("click", function (evt) {
     copy_text("maze-c-array-field");
 });
 
+/* 保存ボタン */
+function download(data, filename, type) {
+    var file = new Blob([data], { type: type });
+    var a = document.createElement("a"),
+        url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(function () {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
+}
+$("#maze-text-save-button").on("click", function (evt) {
+    let maze_string = maze.get_maze_string();
+    download(maze_string, maze_name, "text/plain");
+});
 
 // var maze = new Maze(4);
 // var maze = new Maze(4, [0, 0], [[2, 3], [1, 2]]);
